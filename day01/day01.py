d56dfcaf991deddc48ba5bf3badc03c553f664e9
dani <daniele.soaresl@gmail.com>
2025-03-28 22:10:43 -0300

    Uploading day 01

---
+++ day01/day01.py
@@ -0,0 +1,27 @@
+# Write your code below this line 👇
+from operator import length_hint
+
+#   print("Hello World!\nHello world!")
+#   print("Hello" + " " + "Dani")
+#   print(f"Hello" + + input ("What is your name?") + "!")
+
+#   name = "Dani"
+#   print(name)
+
+# print(len(input("What (is your name?")))
+
+#   username = input("What is your name?")
+#   length = len(username)
+#   print(length)
+
+#   glass1 = "milk"
+#   glass2 = "juice"
+
+#   name = "Dani"
+#   length = len(name)
+#   print(length)
+
+print("Welcome to the Band Name Generator.")
+city = input("Wich city did you grow up in?\n")
+pet = input("What is the name of a pet?\n")
+print("Your band name could be: " + city + " " + pet)
