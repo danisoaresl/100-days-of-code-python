46fa6eed35a87697642ea17bbf7e0e103375685a
dani <daniele.soaresl@gmail.com>
2025-04-22 16:55:28 -0300

    refactor: renomeado arquivo para tip_calculator.py

---
+++ day02/tip_calculator.py
@@ -0,0 +1,76 @@
+# Subscripting
+# print("Hello"[0])
+
+# String
+# print("123" + "345")
+
+# Integer = Whole number
+# print(123 + 345)
+
+# Large Integers
+# print(123_456_789)
+
+# Float = Floating Point Number
+# print(3.14159)
+
+# Boolean
+# print(True)
+# print(False)
+
+# print(int("123") + int("456"))
+
+# int()
+# float()
+# str()
+# bool()
+
+# name_of_the_user = input("Enter your name")
+# length_of_name = len(name_of_the_user)
+
+# print(type("Number of letters in your name: ")) #str
+# print(type(lngth_of_name)) #int
+
+# print("Number of letters in your name: " + str(length_of_name))
+      
+# print("Number of letters in your name: " + len(input("Enter your name")))
+
+# Mathematical Opetarions in Python
+# print("My age: " + str(12))
+# print(123 + 456)
+# print(7 - 3)
+# print(3 * 2)
+# print(5 / 3)
+# print(5 // 3)
+# print(2 ** 3)
+
+# PEMDAS
+
+# ()
+# **
+# * OR /
+# + OR -
+
+# print(3 * 3 + 3 / 3 - 3)
+# print
+
+# bmi = 84 / 1.65 ** 2
+# print(bmi)
+
+# print(int(bmi))
+
+# print(round(bmi))
+
+# print(round(bmi, 2))
+
+score = 0
+height = 1.8
+is_winning = True
+
+print(F"Your score is = {score}")
+
+# User scores a point
+score += 1
+print(score)
+
+# f-strings
+print("Your score is " + str(score))
