dd1c6550b2852ad1ce57df4631d0316c4870d686
dani <limas.dev84@gmail.com>
2025-05-13 17:34:43 -0300

    wip implementing core logic

--- day03/day03.py
+++ day03/day03.py
@@ -16,6 +16,39 @@
 #else:
 #    print("Odd")
 
+
+
+#print("Welcome to Python Pizza Deliveries!")
+#size = input("What size pizza do you want? S, M or L: ")
+#pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
+#extra_cheese = input("Do you want extra cheese? Y or N: ")
+
+# todo: work out how much they need to pay based on their size choice.
+#bill = 0
+#if size == "S":
+#    bill += 15
+#elif size == "M":
+#    bill += 20
+#elif size == "L":
+#    bill += 25
+#else:
+#    print("You typed the wrong inputs.")
+
+# todo: work out how much to add to their bill based on their pepperoni choice.
+#if pepperoni == "Y":
+#    if size == "S":
+#        bill += 2
+#    else:
+#        bill += 3
+
+# todo: work out their final amount on whether if they want extra cheese.
+#if extra_cheese == "Y":
+#    bill += 1
+
+#print(f"Your final bill is: ${bill}.")
+
+
+
 #print("Welcome to the rollercoaster!")
 #height = int(input("What is your height in cm?"))
 #bill = 0
@@ -23,50 +56,32 @@
 #if height >= 120:
 #    print("You can ride the rollercoaster")
 #    age = int(input("What is your age? "))
-#    if age <= 12:
+#    if age < 12:
 #        bill = 5
 #        print("Child tickets are $5.")
 #    elif age <= 18:
 #        bill = 7
 #        print("Youth tickets are $7. ")
+#    elif age >= 45 and age <= 55:
+#        print("Everything is going to be ok. Have a free ride on us!")
 #    else:
 #        bill = 12
-#        print("Adult tickets are $12. ")
+#        print("Adult tickets are $12.")
 
-#    wants_photo = input("Do you want to have a photo take? Type y for Yes and n for No.")
-#    if wants_photo == "y":
-#        # Add $3 to their bill
+#   wants_photo = input("Do you want to have a photo take? Type y for Yes and n for No.")
+#   if wants_photo == "y":
 #        bill += 3 
 
-#    print(f"Your final bill is ${bill}") 
+#   print(f"Your final bill is ${bill}") 
+
 #else:
-#    print("Sorry you have to grow taller before you can ride.")
+#    print("Sorry, you have to grow taller before you can ride.")
 
-print("Welcome to Python Pizza Deliveries!")
-size = input("What size pizza do you want? S, M or L: ")
-pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
-extra_cheese = input("Do you want extra cheese? Y or N: ")
 
-# todo: work out how much they need to pay based on their size choice.
-bill = 0
-if size == "S":
-    bill += 15
-elif size == "M":
-    bill += 20
-elif size == "L":
-    bill += 25
-else:
-    print("You typed the wrong inputs.")
 
-# todo: work out how much to add to their bill based on their pepperoni choice.
-if pepperoni == "Y":
-    if size == "S":
-        bill += 2
-    else:
-        bill += 3
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+choice1 = input('You\'re at a crossroad, where do you want to go? Type "left" or "right".').lowe()_
 
-# todo: work out their final amount on whether if they want extra cheese.
-if extra_cheese == "Y":
-    bill += 1
-
-print(f"Your final bill is: ${bill}.")
+choice1 == "right":
+    print("You fell in to a hole. Game over.")
