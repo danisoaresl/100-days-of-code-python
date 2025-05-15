d330674094365a7bd404745a335999b72167abce
dani <limas.dev84@gmail.com>
2025-05-15 16:17:15 -0300

    test: add tests for day01 to day03 and configure GitHub Actions workflow

---
+++ day01/tests/test_Band Name Generator.py
@@ -0,0 +1,6 @@
+from operator import length_hint
+
+print("Welcome to the Band Name Generator.")
+city = input("Wich city did you grow up in?\n")
+pet = input("What is the name of a pet?\n")
+print("Your band name could be: " + city + " " + pet)
