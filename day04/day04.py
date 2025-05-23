45279bd9483e81f71a0bac68490ce9b308e964a5
dani <limas.dev84@gmail.com>
2025-05-23 17:23:59 -0300

    Uploading an incomplete Day4

---
+++ day04/day04.py
@@ -0,0 +1,37 @@
+## RANDOMISATION
+
+# What do you choose? Type 0 for Rock, 1 for Paper or 2 Scissors.
+
+# import random
+# import my_module
+
+# random_integer = random.randint(a:1, b:10)
+# print(random_integer)
+
+# print(my_module.my_favourite_number)
+
+# random_number_0_to_1 = random.random() * 10
+# print(random_number_0_to_1)
+
+# random_float = random.uniform(a:1, b:10)
+# print(random_float)
+
+# print(random_heads_or_tails)
+
+
+# random_heads_or_tails = random.randint(a:0, b:1)
+# if random_heads_or_tails == 0:
+#    print("Heads")
+# else:
+#    print("Tails")
+
+
+## LIST
+
+states_of_america = ["Delaware", "Pennsylvania", "New Jersey", "Georgia", "Texas", "Florida"]
+
+print(states_of_america[-1])
+
+
+
+
